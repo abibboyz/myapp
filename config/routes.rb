967db5e006a60dda21f59939b69e78ffe73c0999
Abinash <abi.kh03@gmail.com>
2022-09-11 21:40:08 +1000

    Controller generated and routing learned.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+   #Simple Route
+   get 'demo/index'
+ 
+ #Root route
+ #root 'demo#index'
+ 
+ 
+ #Default route
+ #get ':controller(/:action(/:id))'
 end
