13f106627c22f84061550f9eed47143f4c8a8481
Abinash <abi.kh03@gmail.com>
2022-09-11 21:57:02 +1000

    more on routes and .erb files

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,5 +1,20 @@
 class DemoController < ApplicationController
   layout false
   def index
+    render('index')
+
+  end
+  def hello
+    @array=[1,2,3,4,5,6,7,8,9]
+    render('hello')
+  end
+
+  def hello1
+  #  redirect_to(:controller => 'demo', :action => 'index')
+  redirect_to( :action => 'index')
+  end
+
+  def youtube  
+      redirect_to('https://youtube.com/', allow_other_host: true)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
    #Simple Route
    get 'demo/index'
+   get 'demo/hello'
+   get 'demo/hello1'
+   get 'demo/youtube'
+
  
  #Root route
- #root 'demo#index'
+ #root 'demo#index'  #controller#view
  
  
  #Default route
